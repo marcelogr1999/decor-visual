f98b25f205e8ac5db97b88e9113b9621b0eed977
Marcelo Ribeiro <marcelogr1999@gmail.com>
2026-03-14 20:58:22 -0300

    feat: Update Sanity configuration and dependencies, add styled-components, and refine gitignore rules for Sanity build artifacts.

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -4,5 +4,9 @@ export default defineCliConfig({
   api: {
     projectId: 'e6o6k307',
     dataset: 'production'
+  },
+  studioHost: 'decorvisual',
+  deployment: {
+    appId: 'rpc4wrx3ov2p43a9rymiygms',
   }
 })
--- sanity.config.ts
+++ sanity.config.ts
@@ -3,9 +3,10 @@ import { structureTool } from 'sanity/structure'
 import { schema } from './sanity/schema'
 
 export default defineConfig({
-  basePath: '/studio',
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET as string,
+  // Only use basePath when running inside Next.js
+  basePath: process.env.NEXT_PUBLIC_SANITY_STUDIO_IN_NEXTJS === 'true' ? '/studio' : undefined,
+  projectId: 'e6o6k307',
+  dataset: 'production',
   title: 'DECOR Visual CMS',
   schema,
   plugins: [
